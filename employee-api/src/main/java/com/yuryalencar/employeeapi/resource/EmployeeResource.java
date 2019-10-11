111772156d9c14f1be457a1a16590c6bb62eeb88
Yury Alencar Lima <yuryalencar19@gmail.com>
2019-10-11 00:45:15 -0300

    ADDED: Validation in bad request

--- employee-api/src/main/java/com/yuryalencar/employeeapi/resource/EmployeeResource.java
+++ employee-api/src/main/java/com/yuryalencar/employeeapi/resource/EmployeeResource.java
@@ -2,7 +2,6 @@ package com.yuryalencar.employeeapi.resource;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,14 +9,12 @@ import com.yuryalencar.employeeapi.model.Employee;
 import com.yuryalencar.employeeapi.repository.EmployeeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -37,7 +34,7 @@ public class EmployeeResource {
     }
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody Employee employee, HttpServletResponse response) {
+    public ResponseEntity<Employee> create(@RequestBody Employee employee, HttpServletResponse response) {
         Employee employeeCreated = employeeRepository.save(employee);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
                 .buildAndExpand(employeeCreated.getId()).toUri();
@@ -46,9 +43,9 @@ public class EmployeeResource {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> searchEmployeeById(@PathVariable Long id){
-        Optional employee = employeeRepository.findById(id);
-        return employee.isPresent() ? ResponseEntity.ok(employee.get()) : ResponseEntity.notFound().build();
+    public ResponseEntity<Employee> searchEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        return employee != null ? ResponseEntity.ok(employee) : ResponseEntity.notFound().build();
     }
 
 }
