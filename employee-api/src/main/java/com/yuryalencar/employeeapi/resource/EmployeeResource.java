0ee356113d47be79715a35da39de9bda48ca8bca
Yury Alencar Lima <yuryalencar19@gmail.com>
2019-10-11 00:11:32 -0300

    ADDED: Create and search employee by id

--- employee-api/src/main/java/com/yuryalencar/employeeapi/resource/EmployeeResource.java
+++ employee-api/src/main/java/com/yuryalencar/employeeapi/resource/EmployeeResource.java
@@ -1,18 +1,27 @@
 package com.yuryalencar.employeeapi.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import com.yuryalencar.employeeapi.model.Employee;
 import com.yuryalencar.employeeapi.repository.EmployeeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
- * EmployeeResource
- * Created by: Yury Alencar Lima
+ * EmployeeResource Created by: Yury Alencar Lima
  */
 @RestController
 @RequestMapping("/employees")
@@ -22,8 +31,22 @@ public class EmployeeResource {
     private EmployeeRepository employeeRepository;
 
     @GetMapping
-    public List<Employee> list(){
+    public List<Employee> list() {
         return employeeRepository.findAll();
     }
-    
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Employee employee, HttpServletResponse response) {
+        Employee employeeCreated = employeeRepository.save(employee);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(employeeCreated.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(employeeCreated);
+    }
+
+    @GetMapping("/{id}")
+    public Employee searchEmployeeById(@PathVariable Long id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
 }
