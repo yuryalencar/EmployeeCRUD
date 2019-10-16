575a7622130c06765b5a3aca4ac7bc2bbdc1eb83
Yury Alencar Lima <yuryalencar19@gmail.com>
2019-10-16 19:59:57 -0300

    ADDED: remove employees inserts

--- employee-api/src/main/resources/db/migration/V01__create_employees_table.sql
+++ employee-api/src/main/resources/db/migration/V01__create_employees_table.sql
@@ -5,7 +5,3 @@ CREATE TABLE employee(
     email VARCHAR(255) NOT NULL,
     nis_pis VARCHAR(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `employee`(`name`, `last_name`, `email`, `nis_pis`) VALUES ("Yury","Alencar Lima","yuryalencar19@gmail.com","12345678910");
-INSERT INTO `employee`(`name`, `last_name`, `email`, `nis_pis`) VALUES ("Juliana","Mareco Medeiros","julianamareco18@gmail.com","12345678910");
-INSERT INTO `employee`(`name`, `last_name`, `email`, `nis_pis`) VALUES ("Lucas","da Silva Corrêa","eslucascorrea@gmail.com","12345678910");
